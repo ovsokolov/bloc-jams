590d5a23364ec7fea72aecfaf1ff0301c5c27d94
Oleg Sokolov <olegsokolov@Olegs-Air.home>
2016-09-02 18:17:53 -0400

    Checkpoint 9 events: complited

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,9 +1,7 @@
+var pointsArray = document.getElementsByClassName('point');
 
-var animatePoints = function (){
+var animatePoints = function (points){
 
-        var points = document.getElementsByClassName('point');
-    
-    
         var revealPoint = function(n){ 
             points[n].style.opacity = 1;
             points[n].style.transform = "scaleX(1) translateY(0)";
@@ -16,5 +14,23 @@ var animatePoints = function (){
         }   
 
 };
+
+ window.onload = function() {
+    if (window.innerHeight > 950) {
+        animatePoints(pointsArray);
+    }
+    var sellingPoints = document.getElementsByClassName('selling-points')[0];
+    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+    window.addEventListener('scroll', function(event) {
+        if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+            animatePoints(pointsArray);   
+        }
+    });
+ }
+ 
+ 
+ 
+ 
+
 
       
