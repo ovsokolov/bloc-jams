cb205f6953a13abc6f337c1f45eb82f02c3e4d47
Oleg Sokolov <olegsokolov@Olegs-Air.home>
2016-09-06 09:47:49 -0400

    Checkpoint 16 landing page: completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,30 +1,41 @@
-var pointsArray = document.getElementsByClassName('point');
+//var pointsArray = document.getElementsByClassName('point');
 
-var animatePoints = function (points){
-
-        var revealPoint = function(n){ 
-            points[n].style.opacity = 1;
-            points[n].style.transform = "scaleX(1) translateY(0)";
-            points[n].style.msTransform = "scaleX(1) translateY(0)";
-            points[n].style.WebkitTransform = "scaleX(1) translateY(0)";
+//var animatePoints = function (points){
+var animatePoints = function (){
+        //var revealPoint = function(n){ 
+        //    points[n].style.opacity = 1;
+        //    points[n].style.transform = "scaleX(1) translateY(0)";
+        //    points[n].style.msTransform = "scaleX(1) translateY(0)";
+        //    points[n].style.WebkitTransform = "scaleX(1) translateY(0)";
+        var revealPoint = function() {
+             $(this).css({
+                 opacity: 1,
+                 transform: 'scaleX(1) translateY(0)'
+             });
         }
 
-        forEach(pointsArray,revealPoint);
+        //forEach(pointsArray,revealPoint);
+        $.each($(".point"), revealPoint);
 
 };
 
- window.onload = function() {
-    if (window.innerHeight > 950) {
-        animatePoints(pointsArray);
+ $(window).load(function() {
+ //window.onload = function() {
+    //if (window.innerHeight > 950) {
+    if($(window).height() > 950){
+        animatePoints();
     }
-    var sellingPoints = document.getElementsByClassName('selling-points')[0];
-    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
-    window.addEventListener('scroll', function(event) {
-        if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
-            animatePoints(pointsArray);   
+    //var sellingPoints = document.getElementsByClassName('selling-points')[0];
+    //var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+    var scrollDistance = $(".selling-points").offset().top - $(window).height() + 200;
+    //window.addEventListener('scroll', function(event) {
+    $(window).scroll(function(event) {
+        //if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+        if($(window).scrollTop() >= scrollDistance){
+            animatePoints();   
         }
     });
- }
+ });
  
  
  
